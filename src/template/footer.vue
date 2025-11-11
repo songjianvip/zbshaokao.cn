03c644c6a0506e4aade36b8551d863025ef7d1a7
songjianvip <100865332@qq.com>
2025-11-11 10:20:23 +0800

    增加备案标识

--- src/template/footer.vue
+++ src/template/footer.vue
@@ -11,6 +11,14 @@
         <div class="copyright">
           <p>&copy; 2023-2025 淄博烧烤网. 保留所有权利.</p>
           <p>让世界品味淄博，让淄博走向世界</p>
+          <p class="icp-record">
+            <a
+              href="https://beian.miit.gov.cn/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="鲁ICP备20023832号备案信息"
+            >鲁ICP备20023832号</a>
+          </p>
         </div>
       </div>
     </div>
